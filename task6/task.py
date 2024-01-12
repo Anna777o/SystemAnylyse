ce62187836fbf2e8bc22b77d0373abd652e8f657
Anna777o <98017441+Anna777o@users.noreply.github.com>
2024-01-12 11:57:13 +0300

    Update task.py

--- task6/task.py
+++ task6/task.py
@@ -16,14 +16,39 @@ def count_Ts(data):
         res += item ** 3 - item
     return res
 
+def sz(data):
+    res = 0
+    for item in data:
+        if isinstance(item, list):
+            for l2 in item:
+                res += 1
+        else:
+            res += 1
+    return res
+
+def cre_m(data)t:
+    if (type(data) != list):
+        data = json.loads(data)
+    res = []
+    for item in data:
+        if isinstance(item, list):
+            item_sum = 0
+            for l2 in item:
+                item_sum += int(l2)
+            value = item_sum/len(item)
+            for l2 in item:
+                res.append(value)
+        else:
+            res.append(int(item))
+    return res
 
 def task(*rankings: str):
     m = len(rankings)
     rL = [json.loads(r) for r in rankings]
     # [[1, [2, 3], 4, [5, 6, 7], 8, 9, 10], [[1, 2], [3, 4, 5], 6, 7, 9, [8, 10]]]
-    n = 10
-    matrix = [[1, 1.5], [2.5, 1.5], [2.5, 4], [4, 4.], [6, 4], [6, 6], [6, 7], [8, 9], [9, 9], [10, 9]]
-
+    ni = [sz(r) for r in rL]
+    n = ni[0]
+    matrix = np.array([np.array(cre_m(r)) for r in rL]).T
     xi = np.sum(matrix, axis=1)
     xavg = xi.mean()
     sums = np.sum(np.square(xi - xavg))
@@ -40,4 +65,3 @@ def task(*rankings: str):
 str1 = '[1,[2,3],4,[5,6,7],8,9,10]'
 str2 = '[[1,2],[3,4,5],6,7,9,[8,10]]'
 print(task(str1, str2))
-
